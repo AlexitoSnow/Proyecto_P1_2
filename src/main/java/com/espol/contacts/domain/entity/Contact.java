b0e78b2e5600a575ab89d9168b9c94ad0d0ebb21
AlexitoSnow <ganieves@espol.edu.ec>
2025-05-21 23:18:20 -0500

    refactor(todo): add TODO's

--- src/main/java/com/espol/contacts/domain/entity/Contact.java
+++ src/main/java/com/espol/contacts/domain/entity/Contact.java
@@ -1,4 +1,7 @@
 package com.espol.contacts.domain.entity;
 
+// TODO: Implementar el contacto según los atributos principales
+// TODO: Permitir que Contact sea serializable
+// TODO: Implementar un equals según phone
 public class Contact {
 }
