09dbfa17874e0b53b8a6968f6f8f6d7eadd2fed9
AlexitoSnow <ganieves@espol.edu.ec>
2025-05-22 08:07:30 -0500

    feat(entity): implement Contact

--- src/main/java/com/espol/contacts/domain/entity/Contact.java
+++ src/main/java/com/espol/contacts/domain/entity/Contact.java
@@ -1,7 +1,143 @@
 package com.espol.contacts.domain.entity;
 
-// TODO: Implementar el contacto según los atributos principales
-// TODO: Permitir que Contact sea serializable
-// TODO: Implementar un equals según phone
+import com.espol.contacts.domain.entity.enums.ContactType;
+
+import java.util.*;
+
+// TODO: Convertir en serializable
+// TODO: Convertir subclases y clases que usa en serializable
 public class Contact {
+    protected Long id;
+    protected final List<Phone> phones;
+    protected final Set<Address> addresses;
+    protected final Set<ImportantDate> dates;
+    protected final List<SocialMedia> socialMedias;
+    protected String name;
+    protected String notes;
+    protected ContactType contactType;
+    protected Boolean favorite;
+
+    protected Contact() {
+        phones = new ArrayList<>();
+        addresses = new HashSet<>();
+        dates = new HashSet<>();
+        socialMedias = new ArrayList<>();
+        favorite = false;
+    }
+
+    public static Contact build() {
+        return new Contact();
+    }
+
+    public Contact favorite(Boolean favorite) {
+        this.favorite = favorite;
+        return this;
+    }
+
+    public Contact name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Contact addPhone(Phone phone) {
+        this.phones.add(phone);
+        return this;
+    }
+
+    public Contact addAddress(Address address) {
+        this.addresses.add(address);
+        return this;
+    }
+
+    public Contact removeAddress(Address address) {
+        this.addresses.remove(address);
+        return this;
+    }
+
+    public Contact addDate(ImportantDate date) {
+        this.dates.add(date);
+        return this;
+    }
+
+    public Contact removeDate(ImportantDate date) {
+        this.dates.remove(date);
+        return this;
+    }
+
+    public Contact addSocialMedia(SocialMedia socialMedia) {
+        this.socialMedias.add(socialMedia);
+        return this;
+    }
+
+    public Contact removeSocialMedia(SocialMedia socialMedia) {
+        this.socialMedias.remove(socialMedia);
+        return this;
+    }
+
+    public Contact notes(String notes) {
+        this.notes = notes;
+        return this;
+    }
+
+    public Contact contactType(ContactType contactType) {
+        this.contactType = contactType;
+        return this;
+    }
+
+    public Contact id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public Set<ImportantDate> getDates() {
+        return dates;
+    }
+
+    public List<SocialMedia> getSocialMedias() {
+        return socialMedias;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public ContactType getContactType() {
+        return contactType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
 }
