dfaf2833b86e80fbb7cf30be374608d09d0c5350
AlexitoSnow <ganieves@espol.edu.ec>
2025-05-22 08:13:12 -0500

    feat(repository): implement getFavorites, getByType, getByPhone

--- src/main/java/com/espol/contacts/domain/repository/ContactsRepository.java
+++ src/main/java/com/espol/contacts/domain/repository/ContactsRepository.java
@@ -1,6 +1,7 @@
 package com.espol.contacts.domain.repository;
 
 import com.espol.contacts.domain.entity.Contact;
+import com.espol.contacts.domain.entity.enums.ContactType;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +9,10 @@ import java.util.Optional;
 // TODO: Actualizar el tipo de parámetro phone según se defina en Contact
 public interface ContactsRepository {
     List<Contact> getAll();
-    Optional<Contact> getByPhone(long phone);
+    List<Contact> getFavorites();
+    List<Contact> getByType(ContactType type);
+    Optional<Contact> getById(Long id);
+    Optional<Contact> getByPhone(String phone);
     Contact save(Contact contact);
     void delete(Contact contact);
 }
--- src/main/java/com/espol/contacts/infrastructure/repository/ContactsRepositoryImpl.java
+++ src/main/java/com/espol/contacts/infrastructure/repository/ContactsRepositoryImpl.java
@@ -2,11 +2,13 @@ package com.espol.contacts.infrastructure.repository;
 
 import com.espol.contacts.domain.datasource.ContactsDatasource;
 import com.espol.contacts.domain.entity.Contact;
+import com.espol.contacts.domain.entity.enums.ContactType;
 import com.espol.contacts.domain.repository.ContactsRepository;
 import com.espol.contacts.infrastructure.datasource.DevContactsDatasourceImpl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ContactsRepositoryImpl implements ContactsRepository {
     private final ContactsDatasource datasource;
@@ -21,7 +23,22 @@ public class ContactsRepositoryImpl implements ContactsRepository {
     }
 
     @Override
-    public Optional<Contact> getByPhone(long phone) {
+    public List<Contact> getFavorites() {
+        return getAll().stream().filter(Contact::isFavorite).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Contact> getByType(ContactType type) {
+        return getAll().stream().filter(contact -> contact.getContactType().equals(type)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Contact> getById(Long id) {
+        return datasource.getById(id);
+    }
+
+    @Override
+    public Optional<Contact> getByPhone(String phone) {
         return datasource.getByPhone(phone);
     }
 
