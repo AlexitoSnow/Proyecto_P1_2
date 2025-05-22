fac7575e5d729838285b5d5e936edf7967efa501
AlexitoSnow <ganieves@espol.edu.ec>
2025-05-21 23:14:21 -0500

    feat(constants): add directory folder reference

--- src/main/java/com/espol/contacts/config/constants/Constants.java
+++ src/main/java/com/espol/contacts/config/constants/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     private Constants() {}
 
     public static final String APP_NAME = "Contacts App";
+
+    public static final String DIRECTORY_FOLDER = "app/directory/";
 }
