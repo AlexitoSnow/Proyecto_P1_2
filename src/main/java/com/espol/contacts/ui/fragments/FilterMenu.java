779d017718f8156b94a7af0fc7da2b7f85188c32
AlexitoSnow <ganieves@espol.edu.ec>
2025-05-23 18:50:46 -0500

    feat(fragment): add mocked context menus

---
+++ src/main/java/com/espol/contacts/ui/fragments/FilterMenu.java
@@ -0,0 +1,9 @@
+package com.espol.contacts.ui.fragments;
+
+import javafx.scene.control.ContextMenu;
+
+// TODO: implement
+public class FilterMenu extends ContextMenu {
+    public FilterMenu() {
+    }
+}
