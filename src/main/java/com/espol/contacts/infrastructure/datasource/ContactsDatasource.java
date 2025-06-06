63e98d07d5923faa327b712362c6ac3304c5f9f0
AlexitoSnow <ganieves@espol.edu.ec>
2025-06-06 09:06:53 -0500

    perf(datasource): implements contact datasource

--- src/main/java/com/espol/contacts/infrastructure/datasource/ContactsDatasource.java
+++ src/main/java/com/espol/contacts/infrastructure/datasource/ContactsDatasource.java
@@ -1,70 +1,136 @@
 package com.espol.contacts.infrastructure.datasource;
 
 import com.espol.contacts.config.constants.Constants;
+import com.espol.contacts.config.utils.ArrayList;
+import com.espol.contacts.config.utils.List;
 import com.espol.contacts.domain.datasource.BaseDatasource;
 import com.espol.contacts.domain.entity.Contact;
 import com.espol.contacts.domain.entity.Phone;
+import com.espol.contacts.domain.entity.User;
+import com.espol.contacts.infrastructure.exception.DuplicatedContactException;
+import com.espol.contacts.infrastructure.repository.UsersRepositoryImpl;
 
-import java.util.List;
+import java.io.*;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ContactsDatasource implements BaseDatasource<Contact> {
 
-    // TODO: Llamar a deserializeFile()
+    private final User user;
+    private final List<Contact> contacts;
+
+    private static ContactsDatasource instance;
+    private final static Logger LOGGER = Logger.getLogger(ContactsDatasource.class.getName());
+
+    private ContactsDatasource() {
+        this.user = UsersRepositoryImpl.getLoggedUser();
+        List<Contact> deserializedList = deserializeFile();
+        this.contacts = deserializedList != null ? deserializedList : new ArrayList<>();
+    }
+
+    public static ContactsDatasource getInstance() {
+        if (instance == null) {
+            instance = new ContactsDatasource();
+        }
+        return instance;
+    }
+
     @Override
     public List<Contact> getAll() {
-        return List.of();
+        return contacts;
     }
 
-    @Override
-    public Optional<Contact> getById(Long id) {
-        final List<Contact> contacts = deserializeFile();
-        return contacts.stream().filter(contact -> contact.getId().equals(id)).findFirst();
+    public Optional<Contact> getByPhone(Phone phone) {
+        for (Contact contact : contacts) {
+            if (contact.getPhones().contains(phone)) {
+                return Optional.of(contact);
+            }
+        }
+        return Optional.empty();
     }
 
-    @Override
-    public Optional<Contact> getByPhone(String phone) {
-        final List<Contact> contacts = deserializeFile();
-        return contacts.stream().filter(c -> c.getPhones().contains(new Phone(null, phone))).findFirst();
-    }
-
-    // TODO: Serializar la lista actualizada con el nuevo contacto
-    // TODO: Llamar a deserializeFile() para obtener la lista
-    // TODO: Buscar el contacto en la lista
-    // TODO: Validar que el número no exista en otro contacto (puedes usar getByPhone)
-    // TODO: Si el número existe, lanzar la excepción personalizada DuplicatedContactException
-    // TODO: Si el contacto existe, actualizar la referencia (todo puede cambiar menos el Id)
-    // TODO: Si el contacto no existe, darle un nuevo id (generarlo como el siguiente número en la lista) y luego agregarlo a la lista
-    // TODO: Retornar el mismo item recibido, esto sirve para actualizar la UI como validación de "contacto creado"
-    // Como ven, la lógica para actualizar y agregar sigue el mismo flujo
-    // hasta la búsqueda, por eso se considera un solo method para ambos casos
     @Override
     public Contact save(Contact contact) {
-        return null;
+        for (Phone phone : contact.getPhones()) {
+            if (this.getByPhone(phone).isPresent()) {
+                LOGGER.warning("El número de teléfono ya existe en otro contacto.");
+                throw new DuplicatedContactException(phone.getNumber());
+            }
+        }
+        for (int i = 0; i < contacts.size(); i++) {
+            Contact c = contacts.get(i);
+            if (c.equals(contact)) {
+                contacts.set(i, contact);
+                LOGGER.info("Actualizando contacto ID: " + contact.getId());
+                serialize(contacts);
+                return contact;
+            }
+        }
+        contact.setId(contacts.size() + 1L);
+        contacts.addLast(contact);
+        LOGGER.info("Guardando contacto ID: " + contact.getId());
+        serialize(contacts);
+        return contact;
     }
 
-    // TODO: Llamar a deserializeFile() para obtener la lista
-    // TODO: Remover el elemento
-    // TODO: De ser necesario, validar con try-catch en caso de que el parámetro recibido sea null
     @Override
     public void delete(Contact contact) {
+        contacts.remove(contact);
+        serialize(contacts);
     }
 
-    // TODO: Verificar la existencia de la carpeta
-    // TODO: Crear la carpeta del directorio de contactos en caso de no existir
-    // TODO: Retornar la referencia a la carpeta
-    // TODO: Corregir el tipo de dato de retorno
-    private String getFolder() {
-        // Recordar que en esa ruta se almacenan los directorios de contacto
-        final String directories = Constants.DIRECTORY_FOLDER;
-        return directories;
+    private File getFile() {
+        final File file = new File(Constants.DIRECTORY_FOLDER, user.getUsername() + ".contacts");
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists()) {
+            parent.mkdirs();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                String path;
+                try {
+                    path = file.getCanonicalPath();
+                } catch (IOException ex) {
+                    path = file.getAbsolutePath();
+                }
+                LOGGER.log(Level.SEVERE, "Error al crear el archivo: " + path, e);
+                throw new RuntimeException(e);
+            }
+        }
+        return file;
     }
 
-    // TODO: Implementar la deserialización del archivo, pueden usar un nombre Hardcodeado como "dummy-contacts.contacts"
-    // TODO: Llamar a getFolder para obtener la referencia del directorio
-    // TODO: Corregir variable file que obtiene el archivo
     private List<Contact> deserializeFile() {
-        final String file = getFolder() + "dummy-contacts.contacts";
-        return null;
+        File file = getFile();
+        if (file.length() == 0) {
+            LOGGER.info("El archivo de contactos está vacío o se creó recientemente: " + file.getAbsolutePath());
+            return new ArrayList<>();
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            LOGGER.info("Deserializando contactos desde " + file.getCanonicalPath());
+            return (List<Contact>)ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.warning("No se pudo deserializar el archivo de contactos: " + e.getCause());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void serialize(List<Contact> contacts){
+        final File file = getFile();
+        try {
+            LOGGER.info("Serializando contactos en " + file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file.getCanonicalPath()))){
+            oos.writeObject(contacts);
+        }catch(IOException e) {
+            LOGGER.severe("Error al serializar los contactos: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
