ef13eaa09aaaf89dcc1d28c6c433f24e7c840e83
geovanny.nieves <geovanny.nieves@globant.com>
2025-06-07 00:50:26 -0500

    perf: collect most used icons in Icons constants

---
+++ src/main/java/com/espol/contacts/config/constants/Icons.java
@@ -0,0 +1,23 @@
+package com.espol.contacts.config.constants;
+
+// TODO: Search everywhere in the project for Ikons, check the icon literal value, add it here, and replace the Ikon for this Icon constant
+// TODO See the example of the REGULAR_STAR and SOLID_STAR constants below
+
+/**
+ * This class contains constants for icon names used in the application. <p>
+ * It provides a centralized place to manage icon names, making it easier to maintain and update them. <p>
+ * REGULAR ICONS: do not require a solid background.<p>
+ * SOLID ICONS: require a solid background.
+ */
+public class Icons {
+
+    private Icons() {}
+
+    /**
+     * For example, look for the constants in the FontAwesomeRegular class:
+     * @see org.kordamp.ikonli.fontawesome6.FontAwesomeRegular
+     */
+    public static final String REGULAR_STAR = "far-star";
+
+    public static final String SOLID_STAR = "fas-star";
+}
