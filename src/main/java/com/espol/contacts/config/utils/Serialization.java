0d0d466c398c34fdd6e608b9cf9c4fedf2a94194
LeynerPZambrano <leypzam@gmail.com>
2025-06-08 19:13:11 -0500

    feat(): implement serialization for User

--- src/main/java/com/espol/contacts/config/utils/Serialization.java
+++ src/main/java/com/espol/contacts/config/utils/Serialization.java
@@ -1,6 +1,112 @@
 package com.espol.contacts.config.utils;
 
-// TODO: Implement serialization logic for the application.
-// TODO: Implement deserialization logic for the application.
+import com.espol.contacts.config.SessionManager;
+import com.espol.contacts.config.constants.Constants;
+import com.espol.contacts.config.utils.list.ArrayList;
+import com.espol.contacts.config.utils.list.List;
+import com.espol.contacts.domain.entity.Contact;
+import com.espol.contacts.domain.entity.User;
+import com.espol.contacts.infrastructure.datasource.ContactsDatasourceImpl;
+
+import java.io.*;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public abstract class Serialization {
+    private final static Logger LOGGER = Logger.getLogger(Serialization.class.getName());
+
+    private static File getFile() {
+        final User user = SessionManager.getInstance().getCurrentUser();
+        final File file = new File(Constants.DIRECTORY_FOLDER, user.getUsername() + ".contacts");
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists()) {
+            parent.mkdirs();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                String path;
+                try {
+                    path = file.getCanonicalPath();
+                } catch (IOException ex) {
+                    path = file.getAbsolutePath();
+                }
+                LOGGER.log(Level.SEVERE, "Error al crear el archivo: " + path, e);
+                throw new RuntimeException(e);
+            }
+        }
+        return file;
+    }
+
+    public static File getFile(String filename, boolean createIfNotExists) {
+        final File file = new File( Constants.ACCOUNTS_FOLDER + File.separator + filename + ".user");
+        if (createIfNotExists && !file.exists()) {
+            try {
+                if (file.getParentFile() != null && !file.getParentFile().exists()) {
+                    file.getParentFile().mkdirs();
+                }
+                file.createNewFile();
+            } catch (IOException e) {
+                LOGGER.severe("Error creating user file: " + e.getMessage());
+            }
+        }
+        return file;
+    }
+
+
+
+    public static Optional<T> serializeFile(Object content,String fileName,boolean condition){
+        File file = getFile();
+        if (condition) {
+            file = getFile();
+        } else {
+            file = getFile(fileName, true);
+        }
+        try {
+            LOGGER.info("Serializando Contenido en " + file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file.getCanonicalPath()))){
+            oos.writeObject(content);
+        }catch(IOException e) {
+            LOGGER.severe("Error al serializar el contenido: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+
+       return null;
+    }
+
+    public static Optional<User>  deSerializeFile(String fileName, boolean condition){
+        File file;
+        if (condition) {
+            file = getFile();
+        } else {
+            file = getFile(fileName, true);
+        }
+
+
+        if (file.length() == 0) {
+            LOGGER.info("El archivo está vacío o se creó recientemente: " + file.getAbsolutePath());
+            return (T) new ArrayList<>();
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            LOGGER.info("Deserializando desde " + file.getCanonicalPath());
+            return (T)ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.warning("No se pudo deserializar el archivo" + e.getCause());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+
+
 }
--- src/main/java/com/espol/contacts/infrastructure/datasource/UsersDatasourceImpl.java
+++ src/main/java/com/espol/contacts/infrastructure/datasource/UsersDatasourceImpl.java
@@ -1,13 +1,17 @@
 package com.espol.contacts.infrastructure.datasource;
 
+import com.espol.contacts.config.SessionManager;
 import com.espol.contacts.config.constants.Constants;
 import com.espol.contacts.domain.datasource.UsersDatasource;
 import com.espol.contacts.domain.entity.User;
+import com.espol.contacts.config.utils.Serialization;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.logging.Logger;
+
 
 public class UsersDatasourceImpl implements UsersDatasource {
 
@@ -27,7 +31,7 @@ public class UsersDatasourceImpl implements UsersDatasource {
 
     @Override
     public Optional<User> getAuthenticatedUser(String username, String password) {
-        var user = deserializeFile(username);
+        var user = Serialization.deSerializeFile(username,true);
         if (user.isPresent() && user.get().getPassword().equals(password)) {
             return user;
         }
@@ -36,38 +40,15 @@ public class UsersDatasourceImpl implements UsersDatasource {
 
     @Override
     public boolean exists(String username) {
-        return getFile(username, false).exists();
+        final User user = SessionManager.getInstance().getCurrentUser();
+        return Serialization.getFile(username, true).exists();
     }
 
     @Override
     public User save(User user) {
-        return serializeFile(user).orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
+        return Serialization.serializeFile(user,user.getUsername(),true).orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
     }
 
-    // TODO: Implement deserialization logic properly
-    private Optional<User> deserializeFile(String filename) {
-        File file = getFile(filename, false);
-        return Optional.of(new User(filename, "dummyPassword")); // Dummy password for deserialization
-    }
 
-    // TODO: Implement serialization logic properly
-    private Optional<User> serializeFile(User user) {
-        File file = getFile(user.getUsername(), true);
-        return Optional.of(user);
-    }
 
-    private File getFile(String filename, boolean createIfNotExists) {
-        final File file = new File( Constants.ACCOUNTS_FOLDER + File.separator + filename + ".user");
-        if (createIfNotExists && !file.exists()) {
-            try {
-                if (file.getParentFile() != null && !file.getParentFile().exists()) {
-                    file.getParentFile().mkdirs();
-                }
-                file.createNewFile();
-            } catch (IOException e) {
-                LOGGER.severe("Error creating user file: " + e.getMessage());
-            }
-        }
-        return file;
-    }
 }
