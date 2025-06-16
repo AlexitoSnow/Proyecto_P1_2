6146d0d2a146cf7c7f286797de8dc6cb32f6f115
AlexitoSnow <ganieves@espol.edu.ec>
2025-06-16 15:39:21 -0500

    perf: delete gallery directory when a contact not exists anymore

--- src/main/java/com/espol/contacts/domain/datasource/ContactsDatasource.java
+++ src/main/java/com/espol/contacts/domain/datasource/ContactsDatasource.java
@@ -2,9 +2,15 @@ package com.espol.contacts.domain.datasource;
 
 import com.espol.contacts.config.utils.list.List;
 import com.espol.contacts.domain.entity.Contact;
+import com.espol.contacts.domain.entity.Phone;
+
+import java.util.Optional;
 
 public interface ContactsDatasource {
     List<Contact> getAll();
+    List<Contact> getAllByName(String name);
+    Optional<Contact> getByPhone(Phone phone);
+    Optional<Contact> getById(Long id);
     Contact save(Contact entity);
     void delete(Contact entity);
 }
