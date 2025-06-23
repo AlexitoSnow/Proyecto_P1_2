e6cd1abebb98a228a300c4b9b62e942366c00275
AlexitoSnow <ganieves@espol.edu.ec>
2025-06-22 23:02:12 -0500

    feat: add icon

--- src/main/java/com/espol/contacts/App.java
+++ src/main/java/com/espol/contacts/App.java
@@ -3,19 +3,22 @@ package com.espol.contacts;
 import com.espol.contacts.config.LoggerConfig;
 import com.espol.contacts.config.router.AppRouter;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/images/icon.png"))));
         AppRouter.initStage(stage);
     }
 
     public static void main(String[] args) {
-        setUserAgentStylesheet("fluent-light.css");
+        setUserAgentStylesheet("assets/styles/contacts-app.css");
         LoggerConfig.configure();
 
         launch();
