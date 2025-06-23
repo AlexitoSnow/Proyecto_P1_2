a4282b0489e0deb752b8dc5ff1cdabd8e6ce9009
AlexitoSnow <ganieves@espol.edu.ec>
2025-06-23 10:13:22 -0500

    refactor: remove email from User

--- src/main/java/com/espol/contacts/domain/entity/User.java
+++ src/main/java/com/espol/contacts/domain/entity/User.java
@@ -5,21 +5,11 @@ import java.util.Objects;
 
 public class User implements Serializable {
     private String username;
-    private String email;
     private String password;
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
     public User(String username, String password) {
-        this(username, null, password);
-    }
-
-    public String getEmail() {
-        return email;
+        this.username = username;
+        this.password = password;
     }
 
     public String getPassword() {
